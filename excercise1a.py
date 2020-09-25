516bf69b4d9250da09818a012cfc485aa31a0aaf
siphera <salmansiphenkosi@gmail.com>
2020-09-25 16:26:19 +0200

    moved input to the top

--- excercise1a.py
+++ excercise1a.py
@@ -1,9 +1,9 @@
+# declaring my variables
 annual_salary = float(input("enter annual salary: "))
-monthly_salary = annual_salary / 12
-print("monthly net salary: ", round(monthly_salary, 2))
-
 employment_type = input("please enter employment type: ")
-currency = "R"
+
+monthly_salary = annual_salary // 12
+print("monthly net salary: ", round(monthly_salary, 2))
 
 if employment_type.upper() == 'P':
     total = monthly_salary * 0.295
@@ -13,6 +13,6 @@ if employment_type.upper() == 'P':
 elif employment_type.upper() == 'O':
     total = monthly_salary * 0.25
     grand_total = monthly_salary - total
-    print("monthly gross salary: ", currency, round(grand_total, 2))
+    print("monthly gross salary: ", round(grand_total, 2))
 
 # print("monthly net salary: ", monthly_salary)

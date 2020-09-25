0bb6e80189ed2b3405a34c534e32995904cfbc7f
siphera <salmansiphenkosi@gmail.com>
2020-09-25 15:33:38 +0200

    modifed logic

--- excercise1a.py
+++ excercise1a.py
@@ -1,14 +1,18 @@
 annual_salary = float(input("enter annual salary: "))
 monthly_salary = annual_salary / 12
-# print(monthly_salary)
+print("monthly net salary: ", round(monthly_salary, 2))
 
-amount = annual_salary
-if amount >= 100000:
+employment_type = input("please enter employment type: ")
+currency = "R"
+
+if employment_type.upper() == 'P':
     total = monthly_salary * 0.295
     grand_total = monthly_salary - total
-    print("monthly gross: ", grand_total)
+    print("monthly gross: ",  round(grand_total, 2))
 
-elif amount <= 80000:
+elif employment_type.upper() == 'O':
     total = monthly_salary * 0.25
     grand_total = monthly_salary - total
-    print("monthly gross: ", grand_total)
+    print("monthly gross salary: ", currency, round(grand_total, 2))
+
+# print("monthly net salary: ", monthly_salary)
